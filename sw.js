46adccabf44917712ca06836cf3d0d8713f3d58d
meowmeowhyogi-dot <meowmeowhyogi@gmail.com>
2026-08-27 14:42:15 +0800

    刷新离线缓存版本

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jinlongyu-clicker-v1';
+const CACHE_NAME = 'jinlongyu-clicker-v2';
 const OFFLINE_FILES = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
